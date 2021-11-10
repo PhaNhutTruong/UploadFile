02398af84cf99e690dd5869a2fa1969d4d31dd42
PhanNhutTruong <tuong22223@gmail.com>
2021-11-10 08:25:07 +0700

    Add project files.

---
+++ UploadFile/Models/CKEditor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UploadFile.Models
+{
+    public class CKEditor
+    {
+        public string description { get; set; }
+    }
+}
